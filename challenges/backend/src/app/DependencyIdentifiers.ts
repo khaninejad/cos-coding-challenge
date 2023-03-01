645cc6b7b71e19680c71b42d9d9beed571f6fad1
Payam <payam.khaninejad@coinhaven.com>
2023-03-01 13:32:46 +0100

    include service in DI

--- challenges/backend/src/app/DependencyIdentifiers.ts
+++ challenges/backend/src/app/DependencyIdentifiers.ts
@@ -1,3 +1,5 @@
 export const DependencyIdentifier = {
-    LOGGER: "logger"
+    LOGGER: "logger",
+    CONFIG: "config",
+    CALCULATOR: "calculator",
 };
--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -3,6 +3,10 @@ import {ILogger} from "./services/Logger/interface/ILogger";
 import {Logger} from "./services/Logger/classes/Logger";
 import {DependencyIdentifier} from "./DependencyIdentifiers";
 import {AuctionMonitorApp} from "./AuctionMonitorApp";
+import { IConfig } from "./services/Config/interface/IConfig";
+import { Config } from "./services/Config/classes/Config";
+import { ICalculator } from "./services/CarOnSaleClient/interface/ICalculator";
+import { Calculator } from "./services/CarOnSaleClient/classes/Calculator";
 
 /*
  * Create the DI container.
@@ -15,6 +19,8 @@ const container = new Container({
  * Register dependencies in DI environment.
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
+container.bind<IConfig>(DependencyIdentifier.CONFIG).to(Config);
+container.bind<ICalculator>(DependencyIdentifier.CALCULATOR).to(Calculator);
 
 
 /*
